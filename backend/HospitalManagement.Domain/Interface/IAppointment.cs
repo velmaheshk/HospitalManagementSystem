0b6dbb6be596a256dc32a18a678684a58a8e472f
Bhuvanesh Waran <bhuvaneshk0001@gmail.com>
2026-08-08 12:27:48 +0530

    addedinterface

---
+++ backend/HospitalManagement.Domain/Interface/IAppointment.cs
@@ -0,0 +1,12 @@
+﻿using HospitalManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagement.Domain.Interface
+{
+    public interface IAppointment
+    {
+        Task<List<Appointment>> GetAll();
+    }
+}
