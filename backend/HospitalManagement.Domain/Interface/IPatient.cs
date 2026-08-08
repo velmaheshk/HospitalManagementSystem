8bd5382b473c4617455d790726aa5555ea96d229
sjini6198 <sjini6198@gmail.com>
2026-08-08 12:57:53 +0530

    create patient interface

---
+++ backend/HospitalManagement.Domain/Interface/IPatient.cs
@@ -0,0 +1,12 @@
+﻿using HospitalManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalManagement.Domain.Interface
+{
+    public interface IPatient
+    {
+        Task<List<Patient>> GetAll();
+    }
+}
